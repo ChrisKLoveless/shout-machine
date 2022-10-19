83685532bbe5aa5490b47c3bd0fc61ef881f1902
ChrisKLoveless <chriskloveless@gmail.com>
2022-10-19 11:25:11 -0700

    add functions for event handling and phrase uppercase
    co-authored-by: Anahi Ruiz <anahi.sulikey@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function getAndSetShoutValues() {
+    const phraseA = document.getElementById("phraseInput").value;
+    document.querySelector("span#phraseA").innerText = phraseA.toUpperCase();
+}
+
+function setFormSubmissionEventHandler() {
+    let form = document.querySelector("form");
+    form.onsubmit = function (event) {
+        event.preventDefault();
+        getAndSetShoutValues();
+        document.querySelector("div#yell").removeAttribute("class");
+    }
+}
+
+window.onload = function() {
+    setFormSubmissionEventHandler();
+}
